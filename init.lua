489e167e4905179ad6bbfa16325150fd8aa59ea1
Wouter Meek <woutermeek@users.noreply.github.com>
2024-07-11 21:42:19 +0200

    Disable swap files

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- disable swap
+vim.opt.swapfile = false
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
